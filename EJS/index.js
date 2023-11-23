741d81093210bac25e09b42dcbdf44ec1af7def9
ecarfilaf <esteban.carfilaf@sonda.com>
2023-11-23 20:11:48 -0300

    Udemy backend express server v03.001 EJS Partials

--- EJS/index.js
+++ EJS/index.js
@@ -2,6 +2,7 @@
 
 import express from "express";
 import bodyParser from "body-parser";
+import morgan from "morgan";
 
 import { dirname } from "path";
 import { fileURLToPath } from "url";
@@ -12,6 +13,9 @@ const port = 3000;
 
 // set the view engine to ejs
 app.set('view engine', 'ejs');
+
+// // morgan middleware entrega en console datos de solicitudes al server
+//app.use(morgan("tiny"));
 
 app.use(bodyParser.urlencoded({extended: true}));
 
@@ -38,17 +42,37 @@ app.use(bodyParser.urlencoded({extended: true}));
 // 	res.render('index2',data);
 // });
 
-// index3 page three Passing data /*********************/
+// // index3 page three Passing data /*********************/
+// app.get("/", (req, res) => {
+// 	res.render('index3');
+// });
+
+// app.post("/submit", (req, res) => {
+// 	//console.log(req.body);
+// 	const numLetters = req.body["fName"].length + req.body["lName"].length;
+// 	res.render('index3',{letterNumber :numLetters});
+// });
+
+// index3 page fuor Partials /*********************/
+/* Write your code here:
+Step 1: Render the home page "/" index.ejs
+Step 2: Make sure that static files are linked to and the CSS shows up.
+Step 3: Add the routes to handle the render of the about and contact pages.
+  Hint: Check the nav bar in the header.ejs to see the button hrefs
+Step 4: Add the partials to the about and contact pages to show the header and footer on those pages. */
+
+app.use(express.static("public"));
+
 app.get("/", (req, res) => {
-	res.render('index3');
+	res.render('index4');
 });
-
-app.post("/submit", (req, res) => {
-	//console.log(req.body);
-	const numLetters = req.body["fName"].length + req.body["lName"].length;
-	res.render('index3',{letterNumber :numLetters});
+app.get("/about", (req, res) => {
+	res.render('about');
 });
-
+app.get("/contact", (req, res) => {
+	res.render('contact');
+});
+//*********************/*********************/
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });
